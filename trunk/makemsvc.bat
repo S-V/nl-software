a1d3d9e507bf8a4eb96016090141c7821ef817a4
nikolai.zolotykh <nikolai.zolotykh@f8cb7e9f-7c20-0410-8257-d3414394d4c2>
2007-12-28 16:40:05 +0000

    COPYING file added (GPL license)

--- trunk/makemsvc.bat
+++ trunk/makemsvc.bat
@@ -1,6 +1,7 @@
 md tmp
 cl /O2 /Iinclude /Fotmp\ /c src\*.c
 lib /OUT:lib\nl.lib tmp\*.obj
+rem for %%i in (examples\*.c) do cl /O2 /Iinclude /Fotmp\ /Feexamples\ %%i lib\nl.lib libguide40.lib mkl_c.lib
 for %%i in (examples\*.c) do cl /O2 /Iinclude /Fotmp\ /Feexamples\ %%i lib\nl.lib libguide40.lib mkl_c.lib
 del tmp\*.obj
 rmdir tmp
